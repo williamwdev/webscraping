a53a3e81506484311692fd7773108e811dfd9c5e
William Y Wong <wwong1987@gmail.com>
2020-11-18 12:25:40 -0800

    web scrapper to filter out jobs completed with local result storing

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # Web Scrapping
 
+## Overview
+Testing out basic web scrapper using Python. 
+- scrap timejobs.com
+- function will make request from job-search with python as a required skill on timejobs.com
+- function will find all recently posted jobs by 'few' keyword in published date
+- request user input to filter out skills user is unfamiliar with
+- result will be stored locally in separate txt files
+- function will scrape every 10 minutes
+
 ## Setting up python locally
 - [Python on VSCode](https://code.visualstudio.com/docs/python/python-tutorial)
 - `py -3 --version` (Python 3.9.0)
-
-
-## Setting up locally
 - `pip install beautifulsoup4`
 - `pip install lxml`
 - `pip install requests`
+- `pip install time`
+
+## Running it locally
+- open terminal and cd to project dir
+- run `python main.py`
--- main.py
+++ main.py
@@ -18,23 +18,32 @@ from bs4 import BeautifulSoup
 
 # scrapped website and print out company name and skills
 import requests
+import time
 
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+print('Put some skill that you are not familiar with')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
 
-soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
-for job in jobs:
-  published_date = job.find('span', class_ = 'sim-posted').span.text
-  if 'few' in published_date:
-    company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
-    skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
-
-    # print(company_name)
-    print(f'''
-    Company Name: {company_name}
-    Required Skills: {skills}
-    ''')
-
-    print('')
+def find_jobs():
+  html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+  soup = BeautifulSoup(html_text, 'lxml')
+  jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+  for index, job in enumerate(jobs):
+    published_date = job.find('span', class_ = 'sim-posted').span.text
+    if 'few' in published_date:
+      company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+      skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+      more_info = job.header.h2.a['href']
+      if unfamiliar_skill not in skills:
+        with open(f'posts/{index}.txt', 'w') as f:
+          f.write(f"Company Name: {company_name.strip()} \n")
+          f.write(f"Required Skills: {skills.strip()} \n")
+          f.write(f'More Info: {more_info}')
+        print(f'File saved: {index}')
  
- 
+if __name__ == '__main__':
+  while True:
+    find_jobs()
+    time_wait = 10
+    print(f'Waiting {time_wait} minutes...')
+    time.sleep(time_wait * 60)
