d0bf26791912434ddd89553c26ba4c8c1b6f39c0
William Y Wong <wwong1987@gmail.com>
2020-11-18 11:13:48 -0800

    completed implementation to grab course pricing from html page

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ with open('home.html', 'r') as html_file:
   content = html_file.read()
 
   soup = BeautifulSoup(content, 'lxml')
-  print(soup.prettify())
+  # print(soup.prettify())
+
+  # courses_html_tags = soup.find_all('h5')
+  # for course in courses_html_tags:
+  #   print(course.text)
+
+  course_cards = soup.find_all('div', class_='card')
+  for course in course_cards:
+    course_name = course.h5.text
+    course_price = course.a.text.split()[-1]
+    print(f'{course_name} costs {course_price}')
